244cd08994abef8e1d2fb720206bf32e48691892
darkartaudio <mikey@mikeyallred.com>
2023-05-24 23:36:41 -0500

    feat: [profile] add route, send data to view page

--- server.js
+++ server.js
@@ -43,6 +43,11 @@ app.get('/', (req, res) => {
 
 app.use('/auth', require('./controllers/auth'));
 
+app.get('/profile', isLoggedIn, (req, res) => {
+  const { id, name, email } = req.user.get(); 
+  res.render('profile', { id, name, email });
+});
+
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`🎧 You're listening to the smooth sounds of port ${PORT} 🎧`);
